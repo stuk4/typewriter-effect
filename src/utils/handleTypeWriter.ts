5ea98a1fb6a1d3382812a0a21822f5ead658c57a
Bastian Perez <didier.bpp@gmail.com>
2023-02-06 17:33:56 -0300

    agrego efecto blink

--- src/utils/handleTypeWriter.ts
+++ src/utils/handleTypeWriter.ts
@@ -5,12 +5,15 @@ export const handleTypeWriter = (element:Element | null,text:string,interval:num
     if (!element) return;
       let charIndex = 0;
       const handleAddChar = ( ) => {
-        element.classList.add('typewriter');
+        element.classList.add('typewriter',"typewriter-blink");
         element.innerHTML += text[charIndex] === "\n" ? "<br>" : text[charIndex];
         charIndex++;
         if (charIndex < text.length) {
           setTimeout(handleAddChar, interval);
+        }else{
+          element.classList.remove("typewriter-blink");
         }
       }
       handleAddChar();
+    
   }
